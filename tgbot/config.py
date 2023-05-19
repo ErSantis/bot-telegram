4cfceecf6613519e80c45f5be0b097228308fbe7
Elkin <ersantis@uninorte.edu.co>
2023-05-19 14:26:08 -0500

    ok

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,3 +1,3 @@
 # any configuration should be stored here
 
-TOKEN = '6003343308:AAGjpabmxGwvEwSTBtr3bOOM3UaUD1okq_Q' # configure env if you need;
+TOKEN = '6003343308:AAET4D_JbgHFkzm0pLdUGK-a8DgUg03qb9U' # configure env if you need;
