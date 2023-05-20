0d1638bb7126a7c8dfffd955ddad9fa92f3d1974
Elkin Santis <93928718+ErSantis@users.noreply.github.com>
2023-05-19 20:34:00 -0500

    Update config.py

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,3 +1,3 @@
 # any configuration should be stored here
 
-TOKEN = '6003343308:AAET4D_JbgHFkzm0pLdUGK-a8DgUg03qb9U' # configure env if you need;
+TOKEN = '6003343308:AAHyRyp_NtFg8Adav2xOiqdG13tnsWg9VWI' # configure env if you need;
