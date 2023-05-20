8bf1a14aa8eced962f92e8c3024dc5c86c354a9e
Elkin <ersantis@uninorte.edu.co>
2023-05-20 15:09:24 -0500

    new token

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,3 +1,3 @@
 # any configuration should be stored here
 
-TOKEN = '6003343308:AAEmZMH1xem3ZhRXf4o40FKto39WDf5gU68' # configure env if you need;
+TOKEN = '6003343308:AAHglVncPTj3Kiro92fCXDQRrVFQrGNNoXo' # configure env if you need;
